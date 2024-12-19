706b43d76c80cc3fceda2d9b13b6e38799b7c0e0
AlexxelA6 <inteligenciavip19@example.com>
2024-12-19 00:56:46 -0600

    Agregando index.php al repositorio

--- Scripts/MySQL/conexion.php
+++ Scripts/MySQL/conexion.php
@@ -1,14 +1,20 @@
 <?php
 
-$dsn = 'mysql:host=junction.proxy.rlwy.net;port=17617;dbname=railway';
-$user = 'root';
-$password = 'nArnqwCaeoKDfdDPUcgaFkmSJbZplxHc';
+$host = getenv('MYSQLHOST');
+$port = getenv('MYSQLPORT');
+$dbname = getenv('MYSQLDATABASE');
+$user = getenv('MYSQLUSER');
+$password = getenv('MYSQLPASSWORD');
+
+$dsn = "mysql:host=$host;port=$port;dbname=$dbname";
 
 try {
     $conexion = new PDO($dsn, $user, $password);
     $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa a la base de datos.";
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
+
 
 ?>
